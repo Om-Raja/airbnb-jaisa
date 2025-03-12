187f41933a5f23fbb5522951f687759251d5fae2
Om Raja <omraja451@gmail.com>
2025-03-13 00:31:33 +0530

    add login page and login function

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const asyncWrapper = require("../utils/asyncWrapper");
+const passport = require("passport");
 
+//signup
 router.get("/signup", (req, res)=>{
     res.render("user/signup.ejs");
 });
@@ -13,12 +15,22 @@ router.post("/signup", asyncWrapper( async(req, res)=>{
         const newUser = new User({ username, email });
         const registeredUser = await User.register(newUser, password); 
         console.log(registeredUser);
-        req.flash("success", "User registration successful!");
+        req.flash("success", `Welcome ${username} to WanderLust!`);
         res.redirect("/listings");
     }catch(err){
         req.flash("error", err.message);
         res.redirect("/signup");
     }
+}));
+
+//login
+router.get("/login", (req, res)=>{
+    res.render("user/login.ejs");
+});
+
+router.post("/login", passport.authenticate("local", {failureRedirect: "/login", failureFlash: true}), asyncWrapper(async(req, res)=>{
+    req.flash("success", `Welcome back ${req.body.username}`);
+    res.redirect("/listings");
 }));
 
 module.exports = router;
