89b23ff09871c8bdd294dfa6dc4993874082e702
Om Raja <omraja451@gmail.com>
2025-03-14 17:10:23 +0530

    add router.route for listing routes

--- routes/listing.js
+++ routes/listing.js
@@ -5,25 +5,20 @@ const asyncWrapper = require("../utils/asyncWrapper");
 const {isLoggedIn, isListOwner, validateListing} = require("../utils/middlewares");
 const listingController = require("../controllers/listing");
 
-// index route
-router.get("/", asyncWrapper(listingController.indexRoute));
-
 // create route
 // written before /listing/:id otherwise server would consider 'new' is an id
 router.get("/new", isLoggedIn, listingController.getNewListingForm);
 
-router.post("/", isLoggedIn, validateListing, asyncWrapper(listingController.addNewListing));
+router.route("/")
+.get(asyncWrapper(listingController.indexRoute))
+.post(isLoggedIn, validateListing, asyncWrapper(listingController.addNewListing));
 
-//show route
-router.get("/:id", asyncWrapper(listingController.showListing));
+router.route("/:id")
+.get(asyncWrapper(listingController.showListing))
+.put(isLoggedIn, isListOwner, validateListing, asyncWrapper(listingController.updateListing))
+.delete(isLoggedIn, isListOwner, asyncWrapper(listingController.destroyListing));
 
 //edit route
 router.get("/:id/edit", isLoggedIn, isListOwner, asyncWrapper(listingController.editListing));
-
-//patch route
-router.put("/:id", isLoggedIn, isListOwner, validateListing, asyncWrapper(listingController.updateListing));
-
-//delete route
-router.delete("/:id", isLoggedIn, isListOwner, asyncWrapper(listingController.destroyListing));
 
 module.exports = router;
